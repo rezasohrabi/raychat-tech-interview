c35c9419ef0a7d1329ac20832234d52567813538
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-04-11 09:33:53 +0430

    add logNumbersEvenOrOdd function to find out that array numbers is even or odd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import { useEffect } from 'react';
 import './App.css';
 import Quiz from './Components/Quiz';
 import ToggleMessage from './ToggleMessage';
 
+window.loadNumbersBlock = function() {
+
+  function isEvenOrOdd(number) {
+    if(number % 2 === 0) return 'even';
+    
+    return 'odd';
+  }
+
+  function count(number) {
+      let count = 0;
+      while(number != 0) {
+        number = Math.floor(number / 10);
+        ++count;
+      }
+      return count;
+  }
+
+  function logNumbersEvenOrOdd(numbers) {
+      for(let index = 0; index < numbers.length; index++ ) {
+          const number = numbers[index];
+
+          const count = count(number);
+          const determinedEvenOrOdd = isEvenOrOdd(number);
+
+          if(count > 1) {
+            console.log(`${number} has ${count} digits (${determinedEvenOrOdd})`);
+            continue;
+          }
+          console.log(`${number} has ${count} digit (${determinedEvenOrOdd})`);
+      }    
+  }
+  logNumbersEvenOrOdd([32, 5, 76, 98543, 7658])
+
+}
+
 function App() {
+  useEffect(() =>{
+      window.loadNumbersBlock();
+  }, []);
   return (
     <div className="app">
         <ToggleMessage />
