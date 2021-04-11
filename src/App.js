ea538f3d647bfe901b8acf952d026bc82691dc83
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-04-11 11:18:14 +0430

    add findFirstMissingCharacter function to log string first missing alphabet character

--- src/App.js
+++ src/App.js
@@ -24,23 +24,62 @@ window.loadNumbersBlock = function() {
       for(let index = 0; index < numbers.length; index++ ) {
           const number = numbers[index];
 
-          const count = count(number);
+          const numberCount = count(number);
           const determinedEvenOrOdd = isEvenOrOdd(number);
 
-          if(count > 1) {
-            console.log(`${number} has ${count} digits (${determinedEvenOrOdd})`);
+          if(numberCount > 1) {
+            console.log(`${number} has ${numberCount} digits (${determinedEvenOrOdd})`);
             continue;
           }
-          console.log(`${number} has ${count} digit (${determinedEvenOrOdd})`);
+          console.log(`${number} has ${numberCount} digit (${determinedEvenOrOdd})`);
       }    
   }
   logNumbersEvenOrOdd([32, 5, 76, 98543, 7658])
 
 }
 
+window.loadFirstMissingCharacterBlock = function() {
+
+  function initCharactersPresent(alphabet) {
+    const characterPresent = {};
+    for(let i = 0; i < alphabet.length; i++){
+      const charactor = alphabet.charAt(i);
+      characterPresent[charactor] = 0;
+    }
+    return characterPresent;
+  }
+
+  function logFirstMissingCharacter(characterPresent) {
+    for(let character in characterPresent) {
+      if(!characterPresent[character]) {
+        console.log(character);
+        break;
+      }
+    }
+  }
+  
+  function findFirstMissingCharacter(string) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    const characterPresent = initCharactersPresent(alphabet);
+
+    for(let i = 0; i < string.length; i++) {
+      const lowerCaseCharacter = string.charAt(i).toLowerCase();
+      if(characterPresent.hasOwnProperty(lowerCaseCharacter)) {
+        characterPresent[lowerCaseCharacter] += 1;
+      }
+    }
+
+    logFirstMissingCharacter(characterPresent)
+  }
+  findFirstMissingCharacter('raychat');
+
+}
+
 function App() {
   useEffect(() =>{
       window.loadNumbersBlock();
+      window.loadFirstMissingCharacterBlock();
   }, []);
   return (
     <div className="app">
